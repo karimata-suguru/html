f3aaa5fac1c959598106dff23531f04f3655e5e0
karimata-suguru <kids.powers@gmail.com>
2023-08-20 22:28:59 +0900

    task5-1を完成させました。

---
+++ task/task5-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題5-1</title>
+</head>
+<body>
+  <h1>九九の計算</h1>
+<?php
+echo "<table>";
+for ($i = 1; $i <= 9; $i++) {
+    echo "<tr>";
+    for ($j = 1; $j <= 9; $j++) {
+        $anser = $i * $j;
+        echo "<td>{$i} × {$j} = {$anser}</td>　";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
+</body>
+</html>
